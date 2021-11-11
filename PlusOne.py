c9e60ed6ec3b394c7f386cca198fb53888b358b7
Chaeyoon <chaeyoonly@gmail.com>
2021-11-10 19:40:23 -0500

    Update PlusOne.py
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
    
    * for 문 거꾸로 돌릴 때:  for i in reversed(range(n))
    * list 맨 앞에 데이터 추가할 때:  list.insert(0,DATA)

--- PlusOne.py
+++ PlusOne.py
@@ -1,11 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        length = len(digits) - 1
-        while digits[length] == 9:
-            digits[length] = 0
-            length -= 1
-        if(length < 0):
-            digits = [1] + digits
-        else:
-            digits[length] += 1
+        length = len(digits)
+        for i in reversed(range(length)):
+            while digits[i] == 9:
+                digits[i] = 0
+                if(i-1 < 0):
+                    digits.insert(0,1)
+                break
+            else:
+                digits[i] += 1
+                break
+       
         return digits
