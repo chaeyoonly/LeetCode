2fd53160ce710888b9b95e5df61b852a1224ad41
Chaeyoon <chaeyoonly@gmail.com>
2021-11-16 21:41:47 -0500

    Update Contains Duplicate.py

--- Array/Contains Duplicate.py
+++ Array/Contains Duplicate.py
@@ -4,15 +4,14 @@ class Solution:
         i = 0
         j = 1
         length = len(nums) - 1
+        nums.sort()
         while duplicate < 1 and length != 0:
             if(nums[i] == nums[j]):
                 duplicate += 1
-            elif(i == length-1 and j == length):
-                break
             elif(j == length):
-                i += 1
-                j = i + 1
+                break
             else:
+                i += 1
                 j += 1
 
         return False if duplicate < 1 else True
