c3debedcc26ca25293995b7027ba842ba2d798af
Chaeyoon <chaeyoonly@gmail.com>
2022-01-11 09:04:19 -0500

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -11,3 +11,21 @@ class Solution:
             else:
                 i += 1
         return -1
+
+    
+# Discuss
+# Faster than 99.34%
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while(low <= high):
+            mid = int(low + (high - low)/2)
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
