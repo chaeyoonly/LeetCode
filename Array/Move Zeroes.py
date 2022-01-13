fdc054bb4080f1aef07baaea585a23efcf22d88c
Chaeyoon <chaeyoonly@gmail.com>
2022-01-13 09:06:23 -0500

    Update Move Zeroes.py
    https://leetcode.com/problems/move-zeroes/

--- Array/Move Zeroes.py
+++ Array/Move Zeroes.py
@@ -10,3 +10,11 @@ class Solution:
             else:
                 i += 1
                 
+# Discuss
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        zero = 0  # records the position of "0"
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
