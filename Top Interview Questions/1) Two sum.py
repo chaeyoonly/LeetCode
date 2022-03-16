410e1a3aed94eedfdcf9651a4fae1ffec24af3c0
Chaeyoon <chaeyoonly@gmail.com>
2022-03-16 12:56:50 -0400

    Update 1) Two sum.py

--- Top Interview Questions/1) Two sum.py
+++ Top Interview Questions/1) Two sum.py
@@ -9,16 +9,21 @@ class Solution:
                 return [hash_table[complement], i]
 
     
-## Interview practice
+### Interview practice
 
-# Brute force way : inefficient & has a runtime of n squared
-# We can do a lot better than brute force using a concept called complement.
-# Complement : the number derived by subtracting a number from a base number. For example, the tens complement of 8 is 2.
+'''
+Brute force way : inefficient & has a runtime of n squared
+We can do a lot better than brute force using a concept called complement.
+Complement : the number derived by subtracting a number from a base number. For example, the tens complement of 8 is 2.
+'''
 
-# Hash table solution : has an O(n) runtime as well as time complexity
-# We would create a hash table with the key being current elements and the value being the index of that elements
-# and we're storing the index because that's what the question is asking to return.
+'''
+Hash table solution : has an O(n) runtime as well as time complexity
+We would create a hash table with the key being current elements and the value being the index of that elements
+and we're storing the index because that's what the question is asking to return.
+'''
 
 
-
-## Array vs Hash table
+### Hash table
+# f(key) -> hash code -> index -> value
+# 
