c2b823f32e91988cff573199f582a1acebf951f3
Chaeyoon <chaeyoonly@gmail.com>
2022-03-16 01:49:33 -0400

    Update 1) Two sum.py

--- Top Interview Questions/1) Two sum.py
+++ Top Interview Questions/1) Two sum.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}        
+        for i in range(0, len(nums)):
+            if nums[i] in dict.keys():              
+                return [dict[nums[i]],i]            
+            dict[target - nums[i]] = i
+            
+        return(0,len(nums)-1)
+      
+## Hash table
