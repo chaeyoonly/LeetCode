cdadf81a6bfd9f4546aea48c9500ddd7e27b9aaf
Chaeyoon <chaeyoonly@gmail.com>
2022-03-16 02:15:25 -0400

    Update 1) Two sum.py

--- Top Interview Questions/1) Two sum.py
+++ Top Interview Questions/1) Two sum.py
@@ -8,4 +8,10 @@ class Solution:
             
         return(0,len(nums)-1)
       
-## Hash table
+## Hash table interview
+
+# Brute force way : inefficient & has a runtime of n squared
+# We can do a lot better than brute force using a concept called complement.
+# Complement : the number derived by subtracting a number from a base number. For example, the tens complement of 8 is 2.
+
+# Hash table way : time complexity is O of n which is linear time
