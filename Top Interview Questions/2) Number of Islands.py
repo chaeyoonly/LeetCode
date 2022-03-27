c8a30f31caa9a38b97fe2fecf1dbbe82d192e4c6
Chaeyoon <chaeyoonly@gmail.com>
2022-03-27 11:50:25 -0400

    Update 2) Number of Islands.py

--- Top Interview Questions/2) Number of Islands.py
+++ Top Interview Questions/2) Number of Islands.py
@@ -28,3 +28,30 @@ class Solution:
         
         
 # DFS Solution
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # Step 1 : Check for an empty graph
+        if not grid or not grid[0]:
+            return 0  
+        
+        count = 0
+        row, col = len(grid), len(grid[0])
+        
+        # Step 2 : DFS function 
+        def dfs(i, j):            
+            if i<0 or j<0 or i>=row or j>=col or grid[i][j] == '0':
+                return 0
+
+            grid[i][j] = '0'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        # Step 3 : Recursive function
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)                    
+        return count
